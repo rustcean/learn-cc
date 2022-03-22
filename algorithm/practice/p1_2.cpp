ff941b4d43799bbf368d93c91d9a5a48168f933d
“wnanqun” <wnanqun@163.com>
2022-03-22 22:12:20 +0800

    选拔天梯的失败经历

---
+++ algorithm/practice/p1_2.cpp
@@ -0,0 +1,27 @@
+//天梯选拔-
+#include <iostream>
+using namespace std;
+struct data
+{
+    int a;
+    int b;
+    int c;    
+};
+int main()
+{   
+    double re=0.0;
+    int n,m;
+    scanf("%d %d",&n,&m);
+    struct data arr[m];
+    for(int i=0;i<m;i++)
+    {
+        scanf("%d %d %d",&arr[i].a,&arr[i].b,&arr[i].c);
+    }
+    int first=1;
+    if(first==arr[0].a)
+    {   
+        first=arr[0].b;
+        re+=arr[0].c;    
+    }
+    printf("%.3f\n",re);
+}
